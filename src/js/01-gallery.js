c4daa713bb57afca29c244a9e1f9ff7c613e95e9
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-09 16:09:07 +0200

    hw 01 add simpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+function createImgCardsMarkup(images) {
+
+    return images.map(({ preview, original, description }) => {
+    
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+  </li>`
+    
+    }).join('');
+    };
+    
+    const createGalleryList = createImgCardsMarkup(galleryItems);
+    const galleryBoxesRef = document.querySelector('.gallery');
+    galleryBoxesRef.insertAdjacentHTML('beforeend', createGalleryList);
+    
+        const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,} ); 
