a9836b54250c053b8c4b9c2cd5c19fe3e2c31c97
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-10 11:13:39 +0200

    local storage submit feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.getElementById('vimeo-player');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', function(e) {
+
+    localStorage.setItem("videoplayer-current-time", "start a new video");
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 
+const throttle = require('lodash.throttle');
+
+const refFeedBackForm = document.querySelector('.feedback-form');
+const refSubmitForm = document.querySelector('.feedback-form button');
+
+refFeedBackForm.addEventListener('input', onListenerFeedback);
+refSubmitForm.addEventListener('submit', onFormSubmit);
+
+getFeedbackLocalStorage();
+
+function onListenerFeedback(e) {
+    
+    const refFeedBackEmail = document.querySelector('.feedback-form input');
+    const refFeedBackMessage = document.querySelector('.feedback-form textarea');
+    
+    const feedbackObj = {
+    email: refFeedBackEmail.value,
+    message: refFeedBackMessage.value,
+};
+
+localStorage.setItem("feedback-form-state", JSON.stringify(feedbackObj));
+
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    
+};
+
+function getFeedbackLocalStorage() {
+
+        const savedFeedback = localStorage.getItem("feedback-form-state");
+        const parseMessage = JSON.parse(savedFeedback);
+
+        if (parseMessage) {
+            console.log(parseMessage);
+        }
+};
