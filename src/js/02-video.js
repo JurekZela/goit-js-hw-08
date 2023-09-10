28d607d49dc09fa12b0b430759cb77059f6c9606
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-10 12:56:13 +0200

    throttle add video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,11 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player');
     const player = new Player(iframe);
 
-    player.on('timeupdate', function(e) {
-
-    localStorage.setItem("videoplayer-current-time", "start a new video");
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+    player.on('timeupdate', throttle(function(e) {
+        
+        const localStorageMsg = localStorage.setItem("videoplayer-current-time", e.seconds);
+        
+    }, 1000));
