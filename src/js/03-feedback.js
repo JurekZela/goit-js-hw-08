d084009946dc7e7345c35f6f851ac37fdacf7da9
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-10 12:41:02 +0200

    feedback throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,43 @@
+import throttle from 'lodash.throttle';
 
-const throttle = require('lodash.throttle');
+const STORAGE_KEY = 'feedback-form-state';
 
 const refFeedBackForm = document.querySelector('.feedback-form');
-const refSubmitForm = document.querySelector('.feedback-form button');
 
-refFeedBackForm.addEventListener('input', onListenerFeedback);
-refSubmitForm.addEventListener('submit', onFormSubmit);
+const refFeedBackEmail = document.querySelector('.feedback-form input');
+const refFeedBackMessage = document.querySelector('.feedback-form textarea');
 
-getFeedbackLocalStorage();
+refFeedBackForm.addEventListener('input', throttle(onListenerFeedback, 500));
+refFeedBackForm.addEventListener('submit', onFormSubmit);
 
 function onListenerFeedback(e) {
-    
-    const refFeedBackEmail = document.querySelector('.feedback-form input');
-    const refFeedBackMessage = document.querySelector('.feedback-form textarea');
+    e.preventDefault();
     
     const feedbackObj = {
     email: refFeedBackEmail.value,
     message: refFeedBackMessage.value,
 };
 
-localStorage.setItem("feedback-form-state", JSON.stringify(feedbackObj));
+localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackObj));
 
 };
 
 function onFormSubmit(e) {
     e.preventDefault();
     
+    getFeedbackLocalStorage();
+
     e.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
-    
+    localStorage.removeItem(STORAGE_KEY);
 };
 
 function getFeedbackLocalStorage() {
 
-        const savedFeedback = localStorage.getItem("feedback-form-state");
+        const savedFeedback = localStorage.getItem(STORAGE_KEY);
         const parseMessage = JSON.parse(savedFeedback);
 
         if (parseMessage) {
+            
             console.log(parseMessage);
         }
 };
