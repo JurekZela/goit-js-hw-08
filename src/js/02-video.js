7633046352e06cf245cc29b35d2c2a26597a680b
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-10 13:13:58 +0200

    set current time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,8 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.getElementById('vimeo-player');
     const player = new Player(iframe);
 
-    player.on('timeupdate', throttle(function(e) {
-        
-        const localStorageMsg = localStorage.setItem("videoplayer-current-time", e.seconds);
-        
-    }, 1000));
+    player.on('timeupdate', throttle(onSaveCurrentTime, 1000));
+
+    function onSaveCurrentTime(e) {
+        return localStorage.setItem("videoplayer-current-time", e.seconds);
+    };
+
+const getSaveCurrentTime = localStorage.getItem("videoplayer-current-time");
+    player.setCurrentTime(getSaveCurrentTime);
